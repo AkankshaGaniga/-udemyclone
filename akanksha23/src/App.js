49dd627b8053991990cf1e9bedf9869984b26186
AkankshaGaniga <akankshaganiga@gmail.com>
2024-06-03 21:39:04 +0530

    Add files via upload

---
+++ akanksha23/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+import Udemy from './component/Udemy'
+
+function App() {
+
+   
+   
+  return (
+    <div>
+      <Udemy/>
+
+    </div>
+  )
+}
+
+export default App
